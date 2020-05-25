e39efdecdb33d17636037d210c6251355dbed677
Velda Kiara <veldakarimi@gmail.com>
2020-05-26 02:34:08 +0300

    added a function to iterate and add values to a variable

--- scrabble.py
+++ scrabble.py
@@ -9,3 +9,10 @@ points = [1, 3, 3, 2, 1, 4, 2,
 letter_to_points = {key:value for key,value in zip(letters, points)}
 letter_to_points[" "] = 0 
 print(letter_to_points)
+
+def score_word(word):
+    point_total = 0
+    for letter in word:
+        letter_to_points.get(letter, 0)
+        point_total += letter
+        return point_total
