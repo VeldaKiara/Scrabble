3a76c84eee4b729afccae38402a7fadb2781414d
Velda Kiara <veldakarimi@gmail.com>
2020-05-26 03:59:23 +0300

    added another function to call players with points

--- scrabble.py
+++ scrabble.py
@@ -25,3 +25,15 @@ player_to_words = {
   "lexi Con":["ERASER", "BELLY", "HUSKY"],
   "Prof Reader":["ZAP","COMA","PERIOD"],
 }
+player_to_points = { }
+for player,words  in player_to_words.items():
+    player_points = 0
+    for word in words:
+        player_points += score_word(word)
+    player_to_points[player] = player_points
+print(player_to_points)
+    
+  
+  
+    
+  
